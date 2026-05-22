816c4394150cf4f30a26c6094dbe17c6642a09b4
gunjika khurana <gunjikakhurana@gmail.com>
2026-05-22 18:51:39 +0530

    Fix Railway port configuration

--- Program.cs
+++ Program.cs
@@ -17,5 +17,7 @@ var app = builder.Build();
 
 app.UseCors("AllowReact");
 app.MapControllers();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
 
 app.Run();
